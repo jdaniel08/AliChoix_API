8fae96ad327312b1371dab43aad4d5c5c2b0f64a
Alexandre Provost Gagnon <aprovostgagnon@gmail.com>
2021-09-28 21:36:38 -0400

    First commit

---
+++ AliChoixServer/Database/MongoCRUD.cs
@@ -0,0 +1,44 @@
+﻿using AliChoixServer.Model;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AliChoixServer.Database
+{
+    public class MongoCRUD
+    {
+        private readonly string DATABASE_NAME = "off";
+        private readonly string TABLE = "products";
+        private IMongoDatabase db;
+
+        public MongoCRUD()
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(DATABASE_NAME);
+        }
+
+        public void InsertFoodItem(Product product)
+        {
+            //todo
+        }
+
+        public T LoadProductById<T>(string id)
+        {
+            var collection = db.GetCollection<T>(TABLE);
+            var filter = Builders<T>.Filter.Eq("id", id);
+            var product = collection.Find(filter).FirstOrDefault();
+            return product;
+        }
+
+        public async Task<T> LoadProductByIdAsync<T>(string id)
+        {
+            var collection = db.GetCollection<T>(TABLE);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+
+            return collection.FindAsync(filter).Result.First();
+        }
+    }
+}
---
+++ AliChoixServer/Model/Product.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AliChoixServer.Model
+{
+
+    [BsonIgnoreExtraElements]
+    public class Product
+    {
+        [BsonElement("_id")]
+        public string Id { get; set; }
+
+        [BsonElement("_keywords")]
+        public string[] Keywords { get; set; }
+    }
+ }
+
