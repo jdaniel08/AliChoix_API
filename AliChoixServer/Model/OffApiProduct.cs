935ee6c8ac1781fd21df60f7ca079dd271403620
Alexandre Provost Gagnon <aprovostgagnon@gmail.com>
2021-10-02 10:34:05 -0400

    Create our own Database model

---
+++ AliChoixServer/Model/OffApiProduct.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace AliChoixServer.Model
+{
+    public partial class OffApiProduct
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("product")]
+        public OffMongoDbProduct Product { get; set; }
+
+        [JsonProperty("status")]
+        public long Status { get; set; }
+
+        [JsonProperty("status_verbose")]
+        public string StatusVerbose { get; set; }
+    }
+}
